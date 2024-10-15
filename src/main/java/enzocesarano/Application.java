12a82a0479ec213b0f4592aa02da3044bf843715
Diana <dianadorojuc3@gmail.com>
2024-10-15 16:44:15 +0200

    Tutti i metodi per BigliettoDAO

--- src/main/java/enzocesarano/Application.java
+++ src/main/java/enzocesarano/Application.java
@@ -139,7 +139,19 @@ public class Application {
         //*********************** 1. Ottengo i biglietti emmessi in un certo periodo **********************************
         LocalDate inizio = LocalDate.of(2023, 2, 1);
         LocalDate fine = LocalDate.of(2024, 1, 1);
-        List<Biglietto> biglietti = bd.bigliettiPerPeriodo(inizio, fine);
+        List<Biglietto> bigliettiPerPeriodo = bd.bigliettiPerPeriodo(inizio, fine);
+
+        //*********************** 2. Ottengo i biglietti emmessi in un certo periodo e uncerto punto di emissione **********************************
+
+        PuntoDiEmissione puntoDiEmissioneId1 = dd.getEntityById(PuntoDiEmissione.class, "0df5d660-6108-40e3-9660-5508a495640c");
+        List<Biglietto> bigliettiPerPeriodoEPEmissione = bd.bigliettiPerPuntoEPeriodo(puntoDiEmissioneId1, inizio, fine);
+
+
+        //*********************** 3. Totale biglietti emmessi in un certo periodo **********************************
+        long totaleBigliettiPerPeriodo = bd.totBigliettiPerPeriodo(inizio, fine);
+
+        //*********************** 4. Totale biglietti emmessi da un punto di emissione in un certo periodo **********************************
+        long totaleBigliettiPerPunto = bd.totBigliettiPerPEmissione(puntoDiEmissioneId1, inizio, fine);
 
 
         em.close();
--- src/main/java/enzocesarano/dao/BigliettoDAO.java
+++ src/main/java/enzocesarano/dao/BigliettoDAO.java
@@ -1,6 +1,7 @@
 package enzocesarano.dao;
 
 import enzocesarano.entities.Biglietto;
+import enzocesarano.entities.PuntoDiEmissione;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.TypedQuery;
 
@@ -32,5 +33,60 @@ public class BigliettoDAO extends DefaultDAO {
         }
 
         return biglietti;
+    }
+
+    //-------------------Nr. biglietti emessi in un punto di emissione in un determinato periodo
+    public List<Biglietto> bigliettiPerPuntoEPeriodo(PuntoDiEmissione puntoDiEmissione, LocalDate inizio, LocalDate fine) {
+        TypedQuery<Biglietto> query = getEntityManager().createQuery(
+                "SELECT b FROM Biglietto b WHERE b.puntoDiEmissione = :puntoDiEmissione AND b.data_emissione BETWEEN :inizio AND :fine", Biglietto.class);
+        query.setParameter("puntoDiEmissione", puntoDiEmissione);
+        query.setParameter("inizio", inizio);
+        query.setParameter("fine", fine);
+
+        List<Biglietto> biglietti = query.getResultList();
+        if (biglietti.isEmpty()) {
+            System.out.println("Non ci sono biglietti per questo periodo!");
+        } else {
+            System.out.println("I biglietti trovati per questo periodo e punto di emissione: ");
+            for (Biglietto biglietto : biglietti) {
+                System.out.println(biglietto);
+            }
+        }
+        return biglietti;
+    }
+
+
+    //-------------------Nr.totale biglietti emessi in un determinato periodo
+    public long totBigliettiPerPeriodo(LocalDate inizio, LocalDate fine) {
+        TypedQuery<Long> countBQuery = getEntityManager().createQuery(
+                "SELECT COUNT(b) FROM Biglietto b WHERE b.data_emissione BETWEEN :inizio AND :fine", Long.class);
+        countBQuery.setParameter("inizio", inizio);
+        countBQuery.setParameter("fine", fine);
+        long totaleBiglietti = countBQuery.getSingleResult();
+        if (totaleBiglietti == 0) {
+            System.out.println("Il totale dei biglietti per il periodo selezionato è zero.");
+        } else {
+            System.out.println("Il totale dei biglietti emessi nel periodo selezionato è: " + totaleBiglietti);
+        }
+        return totaleBiglietti;
+    }
+
+    //-------------------Nr.totale biglietti emessi in un certo punto di emissione
+    public long totBigliettiPerPEmissione(PuntoDiEmissione puntoDiEmissione, LocalDate inizio, LocalDate fine) {
+        TypedQuery<Long> countQueryPEmissione = getEntityManager().createQuery(
+                "SELECT COUNT(b) FROM Biglietto b WHERE b.puntoDiEmissione = :puntoDiEmissione " +
+                        "AND b.data_emissione BETWEEN :inizio AND :fine", Long.class);
+        countQueryPEmissione.setParameter("puntoDiEmissione", puntoDiEmissione);
+        countQueryPEmissione.setParameter("inizio", inizio);
+        countQueryPEmissione.setParameter("fine", fine);
+        long totaleBigliettiPEm = countQueryPEmissione.getSingleResult();
+        if (totaleBigliettiPEm == 0) {
+            System.out.println("Il totale dei biglietti per il " + puntoDiEmissione.getNome_punto() + " è zero.");
+        } else {
+            System.out.println("Il totale dei biglietti emessi dal " + puntoDiEmissione.getNome_punto() + " è: " + totaleBigliettiPEm);
+        }
+        return totaleBigliettiPEm;
+
+
     }
 }
